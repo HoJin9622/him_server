6d4a5e6292a12ab7e9ef323584bd27846bc34dfa
HoJin9622 <kiss0104040@gmail.com>
2020-11-15 18:54:25 +0900

    feat: 유저 회원가입

---
+++ routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const router = express.Router()
+import { registerUser } from '../controllers/userController.js'
+
+router.route('/').post(registerUser)
+// .get(getUsers)
+// router.post('/login', authUser)
+// router
+//   .route('/profile')
+//   .get(protect, getUserProfile)
+//   .put(protect, updateUserProfile)
+// router
+//   .route('/:id')
+//   .delete(protect, admin, deleteUser)
+//   .get(protect, admin, getUserById)
+//   .put(protect, admin, updateUser)
+
+export default router
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 
+import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
 
 connectDB()
@@ -14,6 +16,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
+
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
