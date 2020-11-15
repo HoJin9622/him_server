85f7cc0791f88086101a4b08578413c741dd890a
HoJin9622 <kiss0104040@gmail.com>
2020-11-15 18:59:26 +0900

    feat: 패스워드 암암호화

--- models/userModel.js
+++ models/userModel.js
@@ -31,14 +31,14 @@ const userSchema = mongoose.Schema(
 //   return await bcrypt.compare(enteredPassword, this.password)
 // }
 
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     next()
-//   }
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
 
-//   const salt = await bcrypt.genSalt(10)
-//   this.password = await bcrypt.hash(this.password, salt)
-// })
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
