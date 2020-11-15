94aa2f03cd70429474be34f590932c0fe87c8039
HoJin9622 <kiss0104040@gmail.com>
2020-11-15 18:39:46 +0900

    feat: 데이터베이스 연결

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -12,5 +19,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running is ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
