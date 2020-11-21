47d20362d96a7681c38b848c77a0af93dbbe61d9
HoJin9622 <kiss0104040@gmail.com>
2020-11-21 19:28:06 +0900

    refactor: 필요없는 코드 제거

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,25 +55,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    Get user profile
-// @route   GET /api/users/profile
-// @access  Private
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id)
-
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    })
-  } else {
-    res.status(404)
-    throw new Error('User not found')
-  }
-})
-
 // @desc    유저 패스워드 변경
 // @route   PUT /api/users/profile
 // @access  Public
